167697b8d5d099fd441af56fe13fdb3fc3a76664
johan.ekelundh@gmail.com <johan.ekelundh@gmail.com>
2020-01-15 08:33:21 +0100

    fix

--- enum/Enum.java
+++ enum/Enum.java
@@ -22,7 +22,7 @@ public class Enum {
         season = Season.WINTER;                   //Sätter värdet på season till WINTER
         System.out.println(season);               //Skriver ut WINTER
  
-        season =  Season.WINTERX;                  //Kraschar programmet, AUTUMN är inte fördefinierad som tillåten variabel i Season
+        season =  Season.AUTUMN;                  //Kraschar programmet, AUTUMN är inte fördefinierad som tillåten variabel i Season
  
     }
 }
